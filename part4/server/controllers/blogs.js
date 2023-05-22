57e6c4222185e121eb763192f2f455bc59661c81
guzmansanchez <guzmangsanchez@gmail.com>
2023-05-22 10:13:25 -0500

    part4 controllet blog.js added index.jsremoved

--- part4/server/controllers/blogs.js
+++ part4/server/controllers/blogs.js
@@ -3,16 +3,16 @@ const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 
-const Blog = require('./models/blogs')
+const Blog = require('../models/blogs')
 
 
 
 const password = process.argv[2]
 
-console.log('password',password)
+console.log('password', password)
 
 const mongoUrl = `mongodb+srv://fullstackopen:${password}@cluster0.nxazoik.mongodb.net/?retryWrites=true&w=majority`
-mongoose.set('strictQuery',false)
+mongoose.set('strictQuery', false)
 mongoose.connect(mongoUrl)
 
 app.use(cors())
